372fac6e7ccfcca2225bba46dc0059ba8207cdb9
Kenrique _Ngwa <72971484+kenrique100@users.noreply.github.com>
2024-06-28 12:59:38 -0400

    updated

--- 0x06-unittests_in_js/0-calcul.js
+++
@@ -1,9 +0,0 @@
-// Calculation function.
-
-function calculateNumber(a, b) {
-    firstInt = Math.round(a);
-    secondInt = Math.round(b);
-    return firstInt + secondInt;
-}
-
-module.exports = calculateNumber;
